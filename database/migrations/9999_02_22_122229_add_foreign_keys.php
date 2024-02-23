ae02af4a44e327bbf7b855986bfe95ac94f9cbfb
saraaffro <saraaffro@gmail.com>
2024-02-23 13:23:42 +0100

    feat: technology and project_technology migration working

--- database/migrations/9999_02_22_122229_add_foreign_keys.php
+++ database/migrations/9999_02_22_122229_add_foreign_keys.php
@@ -16,6 +16,11 @@ return new class extends Migration
         Schema::table('projects', function (Blueprint $table) {
             $table -> foreignId('type_id') -> constrained();
         });
+
+        Schema::table('project_technology', function (Blueprint $table) {
+            $table -> foreignId('project_id') -> constrained();
+            $table -> foreignId('technology_id') -> constrained();
+        });
     }
 
     /**
@@ -28,6 +33,14 @@ return new class extends Migration
         Schema::table('projects', function (Blueprint $table) {
             $table -> dropForeign('projects_type_id_foreign');
             $table -> dropColumn('type_id'); //non per forza necessario
+        });
+
+        Schema::table('project_technology', function (Blueprint $table) {
+            $table -> dropForeign('project_technology_project_id_foreign');
+            $table -> dropColumn('project_id');
+
+            $table -> dropForeign('project_technology_technology_id_foreign');
+            $table -> dropColumn('technology_id');
         });
     }
 };
