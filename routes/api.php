f965288b084337025206b5340aac057a627b7e13
saraaffro <saraaffro@gmail.com>
2024-02-29 10:34:49 +0100

    fix: route group api

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/technologies', [TechnologyController :: class, 'index']);
-Route::post('/technologies', [TechnologyController :: class, 'store']);
+Route:: group(['prefix' => 'technologies'], function (){
+    Route::get('/', [TechnologyController :: class, 'index']);
+    Route::post('/', [TechnologyController :: class, 'store']);
+});
+
